d5f69f3825a7782cd98c394aff499ff6095de550
Danvisible <108751125+Danvisible@users.noreply.github.com>
2023-03-18 15:21:33 +0100

    Fixes

--- order/src/main/java/de/leuphana/order/OrderApplication.java
+++ order/src/main/java/de/leuphana/order/OrderApplication.java
@@ -2,8 +2,11 @@ package de.leuphana.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class OrderApplication {
 		public static void main(String[] args) {
 			SpringApplication.run(OrderApplication.class, args);
