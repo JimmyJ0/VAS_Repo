9c4346ef6ed55574d2f945a7169c5832e85062b1
Danvisible <108751125+Danvisible@users.noreply.github.com>
2023-03-18 15:39:38 +0100

    Order added

---
+++ order/src/main/java/de/leuphana/order/component/behaviour/IOrderService.java
@@ -0,0 +1,12 @@
+package de.leuphana.order.component.behaviour;
+
+import de.leuphana.order.component.structure.Order;
+
+import java.util.List;
+
+public interface IOrderService {
+    Order createOrder(Order order);
+    Order getOrder(Long id) throws Exception;
+    List<Order> getAllOrders(); // Add this method
+    void deleteOrder(Long id);
+}
