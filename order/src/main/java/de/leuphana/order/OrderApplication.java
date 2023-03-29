e7120d96ed23100fc20e38a57473bab5874a20fe
Danvisible <108751125+Danvisible@users.noreply.github.com>
2023-03-29 17:21:14 +0200

    Order Zipkin und Modultests

--- order/src/main/java/de/leuphana/order/OrderApplication.java
+++ order/src/main/java/de/leuphana/order/OrderApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import de.leuphana.order.configuration.OpenTelemetryConfig;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class OrderApplication {
--- order/src/test/java/order/behaviour/OrderServiceTest.java
+++ order/src/test/java/order/behaviour/OrderServiceTest.java
@@ -11,7 +11,6 @@ import static org.mockito.Mockito.when;
 import java.util.ArrayList;
 import java.util.Optional;
 
-import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -45,25 +44,24 @@ public class OrderServiceTest {
         order.setOrderId(1L);
         order.setCustomerId(100);
 
-//        ArrayList<OrderPosition> orderPositions = new ArrayList<>();
-//        orderPositions.add(new OrderPosition());
-//        OrderPosition pos1 = new OrderPosition();
-//        pos1.setArticleId("BK1");
-//        pos1.setArticleQuantity(1);
-//        pos1.setPrice(3.30);
-//        pos1.setOrder(order);
-//        
-//        OrderPosition pos2 = new OrderPosition();
-//        pos2.setArticleId("CD1");
-//        pos2.setArticleQuantity(1);
-//        pos2.setPrice(7.30);
-//        pos2.setOrder(order);
-//        
-//        ArrayList<OrderPosition> orderPositions = new ArrayList<>();
-//        orderPositions.add(pos1);
-//        orderPositions.add(pos2);
+        ArrayList<OrderPosition> orderPositions = new ArrayList<>();
+        orderPositions.add(new OrderPosition());
+        OrderPosition pos1 = new OrderPosition();
+        pos1.setArticleId("BK1");
+        pos1.setArticleQuantity(1);
+        pos1.setPrice(3.30);
+        pos1.setOrder(order);
         
-//        order.setOrderPositions(orderPositions);
+        OrderPosition pos2 = new OrderPosition();
+        pos2.setArticleId("CD1");
+        pos2.setArticleQuantity(1);
+        pos2.setPrice(7.30);
+        pos2.setOrder(order);
+        
+        orderPositions.add(pos1);
+        orderPositions.add(pos2);
+        
+        order.setOrderPositions(orderPositions);
         
     }     
     @Test
