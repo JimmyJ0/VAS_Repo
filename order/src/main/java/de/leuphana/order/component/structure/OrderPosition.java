7b74ccbbae05e4504fdab3fc2c462d809e62df7b
Danvisible <108751125+Danvisible@users.noreply.github.com>
2023-03-29 11:26:51 +0200

    Order Postion bla

--- order/src/main/java/de/leuphana/order/component/structure/OrderPosition.java
+++ order/src/main/java/de/leuphana/order/component/structure/OrderPosition.java
@@ -1,80 +1,80 @@
 package de.leuphana.order.component.structure;
 
-
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
+import de.leuphana.order.configuration.OrderPositionKey;
 import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
 
-@JsonIdentityInfo(
-		  generator = ObjectIdGenerators.PropertyGenerator.class, 
-		  property = "positionId")
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "positionId")
 @Entity
 @Table(name = "order_position")
 public class OrderPosition {
-	
-@Id
-private Long id;
 
-@Column(name = "positionId", nullable = false)
-private Long positionId;
+    @EmbeddedId
+    private OrderPositionKey id;
 
-@Column(name = "articleId", nullable = false)
-private String articleId;
+    @Column(name = "positionId", nullable = false, insertable = false, updatable = false)
+    private Long positionId;
 
-@Column(name = "articleQuantity", nullable = false)
-private int articleQuantity;
+    @Column(name = "articleId", nullable = false)
+    private String articleId;
 
-@Column(name = "price", nullable = false)
-private double price;
+    @Column(name = "articleQuantity", nullable = false)
+    private int articleQuantity;
 
-@ManyToOne
-@JoinColumn(name="orderId")
-private Order order;
+    @Column(name = "price", nullable = false)
+    private double price;
 
-public Long getPositionId() {
-	return positionId;
-}
+    @MapsId("orderId")
+    @ManyToOne
+    @JoinColumn(name="orderId")
+    private Order order;
 
-public void setPositionId(Long positionId) {
-	this.positionId = positionId;
-}
+	public Long getPositionId() {
+		return positionId;
+	}
 
-public String getArticleId() {
-	return articleId;
-}
+	public void setPositionId(Long positionId) {
+		this.positionId = positionId;
+	}
 
-public void setArticleId(String articleId) {
-	this.articleId = articleId;
-}
+	public String getArticleId() {
+		return articleId;
+	}
 
-public int getArticleQuantity() {
-	return articleQuantity;
-}
+	public void setArticleId(String articleId) {
+		this.articleId = articleId;
+	}
 
-public void setArticleQuantity(int articleQuantity) {
-	this.articleQuantity = articleQuantity;
-}
+	public int getArticleQuantity() {
+		return articleQuantity;
+	}
 
-public double getPrice() {
-	return price;
-}
+	public void setArticleQuantity(int articleQuantity) {
+		this.articleQuantity = articleQuantity;
+	}
 
-public void setPrice(double price) {
-	this.price = price;
-}
+	public double getPrice() {
+		return price;
+	}
 
-public Order getOrder() {
-	return order;
-}
+	public void setPrice(double price) {
+		this.price = price;
+	}
 
-public void setOrder(Order order) {
-	this.order = order;
-}
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
 
 }
